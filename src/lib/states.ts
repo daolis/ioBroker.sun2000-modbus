246d769e8345c1b02e8fd6bd5aeaf88e1e1da55b
Stephan <stephan.bechter@gmail.com>
2023-09-24 13:12:02 +0200

    disabled postFetchUpdateHooks

--- src/lib/states.ts
+++ src/lib/states.ts
@@ -249,20 +249,20 @@ export class InverterStates {
             },
         ];
         this.postFetchUpdateHooks = [
-            {
-                interval: UpdateIntervalID.HIGH,
-                hookFn: (adapter: AdapterInstance, toUpdate: Map<string, StateToUpdate>) => {
-                    const powerFromGrid = toUpdate.get('grid.supplyFrom');
-                    const powerActiveInverter = toUpdate.get('activePower');
-                    const totalPowerUse = powerFromGrid?.value + powerActiveInverter?.value;
-                    adapter.log.silly(`PostFetchHook: calculate totalPowerUse ${powerFromGrid?.value}, ${powerActiveInverter?.value}, ${totalPowerUse}`);
-                    const result = new Map();
-                    if (totalPowerUse) {
-                        result.set('totalPowerUse', {id: 'totalPowerUse', value: totalPowerUse})
-                    }
-                    return result;
-                }
-            }
+            // {
+            //     interval: UpdateIntervalID.HIGH,
+            //     hookFn: (adapter: AdapterInstance, toUpdate: Map<string, StateToUpdate>) => {
+            //         const powerFromGrid = toUpdate.get('grid.supplyFrom');
+            //         const powerActiveInverter = toUpdate.get('activePower');
+            //         const totalPowerUse = powerFromGrid?.value + powerActiveInverter?.value;
+            //         adapter.log.silly(`PostFetchHook: calculate totalPowerUse ${powerFromGrid?.value}, ${powerActiveInverter?.value}, ${totalPowerUse}`);
+            //         const result = new Map();
+            //         if (totalPowerUse) {
+            //             result.set('totalPowerUse', {id: 'totalPowerUse', value: totalPowerUse})
+            //         }
+            //         return result;
+            //     }
+            // }
         ];
     }
 
